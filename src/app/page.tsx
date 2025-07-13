8de1646729530a15a9c2b16c7f740aaeaa3f4cf3
jeffreylim24 <jeffreylimhms@gmail.com>
2025-07-13 15:53:58 +0800

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-rose-500">
-        Hello World
-      </p>
+      <Button>
+        Hello Janelle
+      </Button>
     </div>
   );
 };
