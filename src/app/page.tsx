bf25c42e776334447322d2db11535dcc3335714d
jeffreylim24 <jeffreylimhms@gmail.com>
2025-07-21 13:28:54 +0800

    updated button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 import { Button } from "@/components/ui/button"; 
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        Hi Janelle
-      </Button>
-      <Button>
-        Hi Janelle 2
-      </Button>
+    <div className="p-4">
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant="elevated">
+          I am a button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="I am a text area" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+    </div>
     </div>  
   );
 };
