a77aae9cdd2dd2595fabb1fef7c0bdd2acb4903c
jeffreylim24 <jeffreylimhms@gmail.com>
2025-08-21 00:40:04 +0800

    06: Categories Finalization

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -1,15 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import { CustomCategory } from "../types";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
+import { CategoriesSidebar } from "./categories-sidebar";
 
 interface SearchFiltersProps {
-  data: any;
+  data: CustomCategory[];
 }
 
 export const SearchFilters = ({data} : SearchFiltersProps) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className='px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full'>
-      <SearchInput />
-      <Categories data={data} />
+      <CategoriesSidebar 
+        open={isSidebarOpen} 
+        onOpenChange={setIsSidebarOpen} 
+        data={data}
+      />
+      <SearchInput 
+        data={data} 
+        onOpenSidebar={() => setIsSidebarOpen(true)}
+      />
+      <div className='hidden lg:block'>
+        <Categories 
+          data={data} 
+          onOpenSidebar={() => setIsSidebarOpen(true)}
+        />
+      </div>
     </div>
   );
 }
