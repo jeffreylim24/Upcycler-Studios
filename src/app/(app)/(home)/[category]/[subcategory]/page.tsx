1ecac1eccd57f00faba9ac6ee052360489de6ff4
jeffreylim24 <jeffreylimhms@gmail.com>
2025-08-31 16:47:24 +0800

    14: Product List UI

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,26 +1,36 @@
+import { SearchParams } from "nuqs/server";
+
 import { getQueryClient, trpc } from "@/trpc/server";
-import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { loadProductFilters } from "@/modules/products/search-params";
 
 
 interface Props {
   params: Promise<{
     subcategory: string;
   }>;
+  searchParams: Promise<SearchParams>;
 }
 
-const Page = async ({ params }: Props) => {
+const Page = async ({ params, searchParams }: Props) => {
   const { subcategory } = await params;
+  const filters = await loadProductFilters(searchParams);
+
+  // Convert minPrice and maxPrice to numbers if they are strings
+  const parsedFilters = {
+    ...filters,
+    minPrice: filters.minPrice !== null && filters.minPrice !== undefined ? Number(filters.minPrice) : undefined,
+    maxPrice: filters.maxPrice !== null && filters.maxPrice !== undefined ? Number(filters.maxPrice) : undefined,
+  };
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({ category: subcategory }));
+  void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({ category: subcategory, ...parsedFilters }));
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<ProductListSkeleton />}>
-        <ProductList category={subcategory}/>
-      </Suspense>
+      <ProductListView category={subcategory} />
     </HydrationBoundary>
   );
 }
