59faba1011d586c9b5dd3d60d30eee931af2d558
jeffreylim24 <jeffreylimhms@gmail.com>
2025-09-22 16:59:44 +0800

    19: Checkout Page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,13 +5,15 @@ import { productsRouter } from '@/modules/products/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { tagsRouter } from '@/modules/tags/server/procedures';
 import { tenantRouter } from '@/modules/tenants/server/procedures';
+import { checkoutRouter } from '@/modules/checkout/server/procedures';
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
+  tenants: tenantRouter,
+  checkout: checkoutRouter,
   products: productsRouter,
   categories: categoriesRouter,
-  tenants: tenantRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
