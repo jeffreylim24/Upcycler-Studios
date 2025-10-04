a9a235907266837c190d0b683ea30bcc6468faff
jeffreylim24 <jeffreylimhms@gmail.com>
2025-10-04 16:46:59 +0800

    26: General Improvements

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -74,6 +74,9 @@ export const checkoutRouter = createTRPCRouter({
             },
             {
               "tenant.slug": { equals: input.tenantSlug }
+            },
+            {
+              isArchived: { not_equals: true}
             }
           ]
         }
@@ -168,9 +171,14 @@ export const checkoutRouter = createTRPCRouter({
         collection: 'products',
         depth: 2, // Populate 'category', 'image' & 'tenant' & 'tenant.image'
         where: {
-          id: {
-            in: input.ids,
-          }
+          and: [
+            {
+              id: { in: input.ids }
+            },
+            {
+              isArchived: { not_equals: true }
+            }
+          ]
         }
       });
 
